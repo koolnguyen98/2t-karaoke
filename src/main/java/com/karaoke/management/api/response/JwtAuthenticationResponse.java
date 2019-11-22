9894c5eee665bd36cd9ec83de9d23cdebfc936d6
Thien Nguyen <koolnguyen1610@gmail.com>
2019-11-22 13:52:48 +0700

    remove JWT_Type

--- src/main/java/com/karaoke/management/api/response/JwtAuthenticationResponse.java
+++ src/main/java/com/karaoke/management/api/response/JwtAuthenticationResponse.java
@@ -2,7 +2,6 @@ package com.karaoke.management.api.response;
 
 public class JwtAuthenticationResponse {
     private String token;
-    private String tokenType = "Bearer";
 
     public JwtAuthenticationResponse(String token) {
         this.token = token;
@@ -16,11 +15,4 @@ public class JwtAuthenticationResponse {
         this.token = token;
     }
 
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public void setTokenType(String tokenType) {
-        this.tokenType = tokenType;
-    }
 }
